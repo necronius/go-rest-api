aeb613de85b96ec0fad0980347f9a06ff3f25f19
Felipe Olguín <felipe@oslabs.cl>
2021-03-22 16:22:36 -0300

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/necronius/go-rest-api/internal/transport/http"
+)
+
+type App struct{}
+
+// Run - sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Go REST api")
+	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/necronius/go-rest-api
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
